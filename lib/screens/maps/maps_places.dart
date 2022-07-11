ae398003f123b7ca730416e6208c944f2f04ce85
RIYAD <eng.riyadalali93@gmail.com>
2022-07-11 18:25:26 +0300

    added polylines between places

--- lib/screens/maps/maps_places.dart
+++ lib/screens/maps/maps_places.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:google_place/google_place.dart';
 
@@ -16,7 +17,11 @@ class MapsPlaces extends StatefulWidget {
 }
 
 class _MapsPlacesState extends State<MapsPlaces> {
+  String kGoogleApiKey = "AIzaSyA54WuN4cuPPdhHB5hW-ibaYJGF7ZB_1mE"; // google api keys
   late CameraPosition _initalPosition;
+  Map<PolylineId, Polyline> polylines = {};
+  List<LatLng> polylineCoordinates = [];
+  PolylinePoints polylinePoints = PolylinePoints();
   @override
   void initState() {
     super.initState();
@@ -24,6 +29,30 @@ class _MapsPlacesState extends State<MapsPlaces> {
         target: LatLng(widget.startPosition!.geometry!.location!.lat!,
             widget.startPosition!.geometry!.location!.lng!),
         zoom: 14.4746);
+  }
+
+  _addPolyLine() {
+    PolylineId id = PolylineId("poly");
+    Polyline polyline = Polyline(polylineId: id, color: Colors.blue, points: polylineCoordinates);
+    polylines[id] = polyline;
+    setState(() {});
+  }
+
+  _getPolyline() async {
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+        kGoogleApiKey,
+        PointLatLng(widget.startPosition!.geometry!.location!.lat!,
+            widget.startPosition!.geometry!.location!.lng!),
+        PointLatLng(widget.endPosition!.geometry!.location!.lat!,
+            widget.endPosition!.geometry!.location!.lng!),
+        travelMode: TravelMode.driving);
+
+    if (result.points.isNotEmpty) {
+      result.points.forEach((PointLatLng point) {
+        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+      });
+    }
+    _addPolyLine();
   }
 
   @override
@@ -62,11 +91,13 @@ class _MapsPlacesState extends State<MapsPlaces> {
         initialCameraPosition: _initalPosition,
         markers: Set.from(_markers),
         onMapCreated: (GoogleMapController controller) {
-          Future.delayed(
-              Duration(milliseconds: 200),
-              () => controller.animateCamera(CameraUpdate.newLatLngBounds(
-                  MapUtils.boundsFromLatLngList(_markers.map((loc) => loc.position).toList()), 1)));
+          Future.delayed(Duration(milliseconds: 2000), () {
+            controller.animateCamera(CameraUpdate.newLatLngBounds(
+                MapUtils.boundsFromLatLngList(_markers.map((loc) => loc.position).toList()), 1));
+          });
+          _getPolyline();
         },
+        polylines: Set<Polyline>.of(polylines.values),
       ),
     );
   }
